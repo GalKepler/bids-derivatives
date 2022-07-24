19c44773881cb27f9ea7859973208249b72f36a8
GalBenZvi <benzvigal@gmail.com>
2022-07-24 15:49:51 +0300

    passing gha

--- src/bids_derivatives/derivative/derivative.py
+++ src/bids_derivatives/derivative/derivative.py
@@ -4,22 +4,33 @@ from typing import Union
 
 import parse
 
+from bids_derivatives.derivative.messages import PARTICIPANT_MISMATCH
+
 
 class SingleSubjectDerivative:
     """
-    A SingleDerivative is a class that perform queries on the BIDS-App at a single participant level.
+    A SingleDerivative is a class that perform queries on the
+    BIDS-App at a single participant level.
     """
 
     #: Template
     SUBJECT_TEMPLATE = "sub-{subject}"
 
-    def __init__(self, base_dir: Union[str, Path], participant_label: str = None):
-        self.base_dir, self.participant_label = self.validate_participant_label(Path(base_dir), participant_label)
+    def __init__(
+        self, base_dir: Union[str, Path], participant_label: str = None
+    ):
+        (
+            self.base_dir,
+            self.participant_label,
+        ) = self.validate_participant_label(Path(base_dir), participant_label)
 
     @staticmethod
-    def validate_participant_label(base_dir: Path, participant_label: str = None):
+    def validate_participant_label(
+        base_dir: Path, participant_label: str = None
+    ):
         """
-        Validate the participant label from either a subject-specific directory or a specified label.
+        Validate the participant label from either a subject-specific directory
+        or a specified label.
 
         Parameters
         ----------
@@ -39,19 +50,28 @@ class SingleSubjectDerivative:
             _description_
         """
         if participant_label is not None:
-            parser = parse.parse(SingleSubjectDerivative.SUBJECT_TEMPLATE, participant_label)
+            parser = parse.parse(
+                SingleSubjectDerivative.SUBJECT_TEMPLATE, participant_label
+            )
             if parser:
                 participant_label = parser.named.get("subject")
             else:
                 pass  # Keep participant label as is.
         base_dir_name = base_dir.name
-        parser = parse.parse(SingleSubjectDerivative.SUBJECT_TEMPLATE, base_dir_name)
+        parser = parse.parse(
+            SingleSubjectDerivative.SUBJECT_TEMPLATE, base_dir_name
+        )
         if parser:
             base_dir = base_dir.parent
             if not participant_label:
                 participant_label = parser.named.get("subject")
             else:
                 if participant_label != parser.named.get("subject"):
-                    warnings.warn(f"Participant label {participant_label} does not match the base directory name {base_dir_name}.")
+                    warnings.warn(
+                        PARTICIPANT_MISMATCH.format(
+                            participant_label=participant_label,
+                            base_dir_name=base_dir_name,
+                        )
+                    )
 
         return base_dir, participant_label
---
+++ src/bids_derivatives/derivative/messages.py
@@ -0,0 +1,1 @@
+PARTICIPANT_MISMATCH = "Participant label {participant_label} does not match the base directory name {base_dir_name}."
