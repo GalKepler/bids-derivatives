81cded346268e199ac0c190f48d479dcd39a29ec
GalBenZvi <benzvigal@gmail.com>
2022-08-02 21:12:59 +0300

    WIP outputs query

---
+++ src/bids_derivatives/bids_apps/outputs/templates/__init__.py
@@ -0,0 +1,3 @@
+from bids_derivatives.bids_apps.outputs.templates.spaces import (  # noqa: F401
+    STANDARD_SPACES,
+)
---
+++ src/bids_derivatives/bids_apps/outputs/templates/spaces.py
@@ -0,0 +1,3 @@
+from templateflow import api as tflow
+
+STANDARD_SPACES = tflow.templates()
